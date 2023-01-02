27ad037130e052dea6058c616151eed7d47e89f1
Jerrssey <fankton@gmail.com>
2023-01-02 12:02:48 +0200

    two versions - scroll and button

--- src/js/getRefs.js
+++ src/js/getRefs.js
@@ -2,6 +2,6 @@ export default function getRefs() {
   return {
     searchForm: document.querySelector('.search-form'),
     galleryContainer: document.querySelector('.gallery'),
-    fetchButton: document.querySelector('[data-fetch]'),
+    // fetchButton: document.querySelector('[data-fetch]'),
   };
 }
